e8fb5fa9c8f1c094a846c2ed49dcb6839fa34b7e
Matthew Zipkin <pinheadmz@gmail.com>
2019-10-03 12:25:27 -0400

    truncate log during operation
    Co-authored-by: Braydon Fuller <courier@braydon.com>

--- test/logger-test.js
+++ test/logger-test.js
@@ -4,7 +4,18 @@
 'use strict';
 
 const assert = require('bsert');
+const {tmpdir} = require('os');
+const Path = require('path');
+const fs = require('fs');
+const pfs = require('../lib/fs'); // Has promisified stat, but no mkdir
 const Logger = require('../lib/logger');
+
+function tempFile(name) {
+  const time = Date.now();
+  const dir = Path.join(tmpdir(), `blgr-test-${time}`);
+  fs.mkdirSync(dir);
+  return Path.join(dir, `${name}.log`);
+};
 
 describe('Logger', function() {
   describe('log', function() {
@@ -241,6 +252,43 @@ describe('Logger', function() {
       assert.equal(called.level, Logger.levels.SPAM);
       assert.equal(called.args.length, 1);
       checkMsg(called.args[0], true, false);
+    });
+  });
+
+  describe('file size', function() {
+    const actualSize = Logger.MAX_FILE_SIZE;
+    const actualThreshold = Logger.TRUNCATE_THRESHOLD;
+    const testSize = 1 * 1000 * 1000; // 1 MiB
+    const testThreshold = testSize / 10;
+    const filename = tempFile('file-size');
+    let logger;
+
+    before(async () => {
+      Logger.MAX_FILE_SIZE = testSize;
+      Logger.TRUNCATE_THRESHOLD = testThreshold;
+
+      logger = new Logger({
+        level: 'spam',
+        filename: filename,
+        console: false
+      });
+      await logger.open();
+    });
+
+    after(async () => {
+      await logger.close();
+      Logger.MAX_FILE_SIZE = actualSize;
+      Logger.TRUNCATE_THRESHOLD = actualThreshold;
+    });
+
+    it('should keep log file under the size limit while running', async () => {
+      for (let i = 0; i < 10000; i++) {
+        logger.debug(`${i}`);
+        // 500 bytes = 1000 char hex = 1000 bytes written to log file
+        logger.debug(Buffer.alloc(500).toString('hex'));
+      }
+      const stat = await pfs.stat(filename);
+      assert(stat.size <= (testSize + testThreshold));
     });
   });
 });
